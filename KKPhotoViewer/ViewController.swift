ff67d862000dbe0103ee5fd6d2869a5178d66c33
kawamacpr <kawamuramac2018@gmail.com>
2018-12-12 15:44:38 +0900

    Initial Commit

---
+++ KKPhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KKPhotoViewer
+//
+//  Created by Kawamura Kazuhiro on 2018/12/12.
+//  Copyright © 2018 Kawamura Kazuhiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
