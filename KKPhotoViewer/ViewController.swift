9b2b40ea0263c6245417116010866b9f35d980dc
kawamacpr <kawamuramac2018@gmail.com>
2019-01-18 16:19:38 +0900

    2019.01.18 collectionView OK

--- KKPhotoViewer/ViewController.swift
+++ KKPhotoViewer/ViewController.swift
@@ -1,25 +1,48 @@
-//
-//  ViewController.swift
-//  KKPhotoViewer
-//
-//  Created by Kawamura Kazuhiro on 2018/12/12.
-//  Copyright © 2018 Kawamura Kazuhiro. All rights reserved.
-//
-
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
-
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImageView: UIImageView!
+}
+ 
+class ViewController: UICollectionViewController {
+    var fetchResult: PHFetchResult<AnyObject>!
+    var imageManager: PHCachingImageManager!
+    var cellSize: CGSize!
+    
+    override func awakeFromNib() {
+        fetchResult = PHAsset.fetchAssets(with: nil) as! PHFetchResult<AnyObject>
+        imageManager = PHCachingImageManager()
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+       }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.fetchResult.count
+    }
+    
+    override func collectionView(_ collectionView:
+        UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath) as! CollectionViewCell
+        if (cellSize == nil) {
+            let imageBounds = cell.myImageView.bounds
+            cellSize = CGSize(width: imageBounds.size.width, height: imageBounds.size.height)
+        }
+        
+        let asset = self.fetchResult[indexPath.item] as! PHAsset
+        self.imageManager.requestImage(for: asset, targetSize: cellSize, contentMode: .aspectFill, options: nil) {
+            (image:UIImage!, info) ->
+              Void in
+            cell.myImageView.image = image
+        }
+    
+    return cell
+    }
 }
 
